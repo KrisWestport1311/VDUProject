e3181721c2f71a9a337d2e0b1fc8652d2dc6f9a3
KrisWestport <95048498+KrisWestport@users.noreply.github.com>
2022-10-05 22:48:51 +0100

    Setup react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      This is a test Kristian
         
     </div>
   );
