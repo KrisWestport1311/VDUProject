2414096867d2b6881ec4ff65aebffbf8bf950f94
KrisWestport <95048498+KrisWestport@users.noreply.github.com>
2022-10-06 21:36:09 +0100

    Install React Router Dom and test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom"
 
-
+import DashboardHome from "./client/dashboard/pages/home/DashboardHome";
+import Login from "./client/frontend/pages/login/Login"
 
 function App() {
   return (
     <div className="App">
-      This is a test Kristian
+      <Routes>
+        <Route path="/" element={<DashboardHome/>} />    
+        <Route path="login" element={<Login/>} />
+
+      </Routes>
         
     </div>
   );
---
+++ src/client/dashboard/pages/home/DashboardHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./DashboardHome.scss";
+
+const DashboardHome = () => {
+  return (
+    <div className='home'>
+    This is the dashbaord page
+  
+    </div>
+  )
+}
+
+export default DashboardHome
