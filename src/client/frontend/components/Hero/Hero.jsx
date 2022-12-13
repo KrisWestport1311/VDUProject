6bfaceb485b01ef409cfcd9b4f3833c476f5ba99
KrisWestport <95048498+KrisWestport@users.noreply.github.com>
2022-12-13 21:59:26 +0000

    Deploy and updates working

--- src/client/frontend/components/Hero/Hero.jsx
+++ src/client/frontend/components/Hero/Hero.jsx
@@ -1,33 +1,31 @@
-import './hero.scss'
-import HeroVideo from '../../../common/video.mp4'
-import { NavLink } from 'react-router-dom'
+import "./hero.scss";
+import HeroVideo from "../../../common/video.mp4";
+import { NavLink } from "react-router-dom";
 const Hero = () => {
   return (
-    <div className='hero'>
-        <video autoPlay loop muted id='video'>
-            <source src={HeroVideo} type='video/mp4'/>
-        </video>
+    <div className="hero">
+      <video autoPlay loop muted id="video">
+        <source src={HeroVideo} type="video/mp4" />
+      </video>
 
-        <div className="herotxt">
-          <h1 style={{color: "var(--secondary-dark)"}}>Digital Workstation Assessments1</h1>
-          <h2 style={{color: "var(--white)"}}>Remote Assessment Solution</h2>
-          
+      <div className="herotxt">
+        <h1 style={{ color: "var(--secondary-dark)" }}>
+          Digital Workstation Assessments
+        </h1>
+        <h2 style={{ color: "var(--white)" }}>Remote Assessment Solution</h2>
 
-          <div className="btn-group">
-            
-            
-            <NavLink 
-            to="/login" 
-            className="btn">Log In </NavLink> 
-                     
-            <NavLink 
-            to="/contactus" 
-            className="btn btn-outline">Send a Message </NavLink>          
-          </div>
+        <div className="btn-group">
+          <NavLink to="/login" className="btn">
+            Log In{" "}
+          </NavLink>
+
+          <NavLink to="/contactus" className="btn btn-outline">
+            Send a Message{" "}
+          </NavLink>
         </div>
-        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
